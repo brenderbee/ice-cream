cb0adeca8c34fe5a8fc9114dbe750553b586f4cd
Brenda Franco and Matt Dowdney <matt@>
2018-03-26 11:01:52 -0700

    setup project and add initial js logic

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  var flavors = ["chocolate", "vanilla", "strawberry", "mint chocolate chip", "lavender", "rocky road", "cookies 'n' cream", "coffee"];
+  flavors.forEach(function(flavor){
+  $(".jumbotron").append(flavor);
+  });
+});
